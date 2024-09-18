42020ac43122f1eac9ef1c4dd2140ab333c2a634
이경현 <lkh6035@naver.com>
2024-09-18 21:55:28 +0900

    [feat] 240918 회원가입 창 수정 및 개인정보 동의, 이용약관 checkbox 추가

--- app/components/LoginModal.tsx
+++ app/components/LoginModal.tsx
@@ -93,6 +93,7 @@ const LoginModal: React.FC<LoginModalProps> = ({ show, onClose, onLoginSuccess }
                         type="email"
                         value={username}
                         autoComplete="email"
+                        placeholder="email@email.com"
                         required
                         onChange={(e) => setUsername(e.target.value)}
                         className={`block w-full focus:outline-none rounded-md border-0 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-orange-600 sm:text-sm sm:leading-6 ${
@@ -119,6 +120,7 @@ const LoginModal: React.FC<LoginModalProps> = ({ show, onClose, onLoginSuccess }
                         autoComplete="current-password"
                         required
                         value={password}
+                        placeholder="비밀번호"
                         onChange={(e) => setPassword(e.target.value)}
                         className={`block w-full focus:outline-none rounded-md border-0 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-orange-600 sm:text-sm sm:leading-6 ${
                             errorMessage && 'border-red-500'
--- app/components/SignupModal.tsx
+++ app/components/SignupModal.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import ModalLayout from './ModalLayout';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye, faEyeSlash } from '@fortawesome/free-regular-svg-icons';
 
 interface SignupModalProps {
   show: boolean;
@@ -9,14 +11,44 @@ interface SignupModalProps {
 const SignupModal: React.FC<SignupModalProps> = ({ show, onClose }) => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
   const [nickname, setNickname] = useState('');
-  const [terms, setTerms] = useState(false);
-  const [errors, setErrors] = useState({ username: '', password: '', nickname: '' });
+  const [agreeToPrivacy, setAgreeToPrivacy] = useState(false);
+  const [agreeToTerms, setAgreeToTerms] = useState(false);
+  const [errors, setErrors] = useState({
+    username: '',
+    password: '',
+    confirmPassword: '',
+    nickname: '',
+    terms: '',
+  });
+
+  // State for toggling password visibility
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
 
-    setErrors({ username: '', password: '', nickname: '' }); // Reset errors
+    setErrors({ username: '', password: '', confirmPassword: '', nickname: '', terms: '' });
+
+    // Check if passwords match before submitting
+    if (password !== confirmPassword) {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        confirmPassword: '비밀번호가 일치하지 않습니다.',
+      }));
+      return;
+    }
+
+    // Check if the user has agreed to both conditions
+    if (!agreeToPrivacy || !agreeToTerms) {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        terms: '필수 동의 사항에 체크해주세요.',
+      }));
+      return;
+    }
 
     const response = await fetch(`${process.env.NEXT_PUBLIC_USER_API_URL}/accounts/sign-up/`, {
       method: 'POST',
@@ -27,7 +59,6 @@ const SignupModal: React.FC<SignupModalProps> = ({ show, onClose }) => {
         username,
         password,
         nickname,
-        terms,
       }),
     });
 
@@ -40,67 +71,190 @@ const SignupModal: React.FC<SignupModalProps> = ({ show, onClose }) => {
   };
 
   return (
-      <ModalLayout show={show} onClose={onClose}>
-        <div className="relative bg-white p-8 shadow-md w-full h-full sm:rounded-2xl sm:w-96 sm:h-[70%]">
-          <button onClick={onClose} className="absolute top-2 right-2 text-gray-600">X</button>
-          <h2 className="text-2xl mb-4">픽앤팝 회원 가입하기</h2>
-          <p className="mb-4">환영합니다!</p>
-          <form onSubmit={handleSubmit}>
-            <label htmlFor="username" className="block text-sm font-medium text-gray-700">이메일</label>
-            <input
-                type="email"
-                id="username"
-                className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2"
-                placeholder="이메일을 입력하세요"
-                value={username}
-                onChange={e => setUsername(e.target.value)}
-                required
+    <ModalLayout show={show} onClose={onClose}>
+      <div className="relative bg-white p-8 shadow-md w-full h-full sm:rounded-2xl sm:w-96 sm:h-auto">
+        <button onClick={onClose} className="absolute top-2 right-2 text-gray-600">X</button>
+        <form className="flex min-h-full flex-col justify-center px-6 lg:px-8" onSubmit={handleSubmit}>
+          <div className="flex flex-row sm:mx-auto sm:w-full sm:max-w-sm">
+            <img
+                className="mx-auto h-8 w-auto"
+                src="https://tailwindui.com/img/logos/mark.svg?color=orange&shade=600"
+                alt="Your Company"
+                width={20}
+                height={20}
             />
-            {errors.username && <p className="text-red-500 text-sm">{errors.username}</p>}
+          </div>
+          <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-sm">
+            <div className="space-y-2">
+              <div>
+                <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
+                  이메일<span className="pl-1 text-red-800">*</span>
+                </label>
+                <div className="mt-2">
+                  <input
+                      type="email"
+                      id="username"
+                      value={username}
+                      placeholder="email@email.com"
+                      required
+                      onChange={(e) => setUsername(e.target.value)}
+                      className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2"
+                  />
+                  {errors.username && <p className="text-red-500 text-sm">{errors.username}</p>}
+                </div>
+              </div>
 
-            <label htmlFor="password" className="block text-sm font-medium text-gray-700 mt-4">비밀번호</label>
-            <input
-                type="password"
-                id="password"
-                className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2"
-                placeholder="비밀번호를 입력하세요"
-                value={password}
-                onChange={e => setPassword(e.target.value)}
-                required
-            />
-            {errors.password && <p className="text-red-500 text-sm">{errors.password}</p>}
+              <div>
+                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                  비밀번호<span className="pl-1 text-red-800">*</span>
+                </label>
+                <div className="relative mt-2">
+                  <input
+                      type={showPassword ? 'text' : 'password'}
+                      id="password"
+                      required
+                      value={password}
+                      placeholder="비밀번호"
+                      onChange={(e) => setPassword(e.target.value)}
+                      className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2"
+                  />
+                  <button
+                      type="button"
+                      onClick={() => setShowPassword(!showPassword)}
+                      className="absolute inset-y-0 right-2 flex items-center text-gray-500"
+                  >
+                    <FontAwesomeIcon icon={showPassword ? faEye : faEyeSlash}/>
+                  </button>
+                  {errors.password && <p className="text-red-500 text-sm">{errors.password}</p>}
+                </div>
+              </div>
 
-            <label htmlFor="nickname" className="block text-sm font-medium text-gray-700 mt-4">닉네임</label>
-            <input
-                type="text"
-                id="nickname"
-                className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2"
-                placeholder="닉네임을 입력하세요"
-                value={nickname}
-                onChange={e => setNickname(e.target.value)}
-                required
-            />
-            {errors.nickname && <p className="text-red-500 text-sm">{errors.nickname}</p>}
+              <div>
+                <label htmlFor="confirm-password" className="block text-sm font-medium leading-6 text-gray-900">
+                  비밀번호 확인<span className="pl-1 text-red-800">*</span>
+                </label>
+                <div className="relative mt-2">
+                  <input
+                      type={showConfirmPassword ? 'text' : 'password'}
+                      id="confirm-password"
+                      required
+                      value={confirmPassword}
+                      placeholder="비밀번호 확인"
+                      onChange={(e) => setConfirmPassword(e.target.value)}
+                      className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2"
+                  />
+                  <button
+                      type="button"
+                      onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+                      className="absolute inset-y-0 right-2 flex items-center text-gray-500"
+                  >
+                  </button>
+                  {errors.confirmPassword && <p className="text-red-500 text-sm">{errors.confirmPassword}</p>}
+                </div>
+              </div>
 
-            <label className="block mt-4">
-              <input
-                  type="checkbox"
-                  id="terms"
-                  className="mr-2"
-                  checked={terms}
-                  onChange={e => setTerms(e.target.checked)}
-                  required
-              /> 픽앤팝 운영 정책에 동의합니다.
-            </label>
+              <div>
+                <label htmlFor="nickname" className="block text-sm font-medium leading-6 text-gray-900">
+                  닉네임<span className="pl-1 text-red-800">*</span>
+                </label>
+                <div className="mt-2">
+                  <input
+                      type="text"
+                      id="nickname"
+                      required
+                      value={nickname}
+                      placeholder="닉네임 입력"
+                      onChange={(e) => setNickname(e.target.value)}
+                      className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2"
+                  />
+                  {errors.nickname && <p className="text-red-500 text-sm">{errors.nickname}</p>}
+                </div>
+              </div>
 
-            <button type="submit" className="w-full bg-blue-500 text-white py-2 mt-4 rounded-md hover:bg-blue-600">
-              회원 가입하기
-            </button>
-            {/* <button type="button" className="w-full bg-red-500 text-white py-2 mt-2 rounded-md hover:bg-red-600">구글계정으로 가입하기</button> */}
-          </form>
-        </div>
-      </ModalLayout>
-);
+              <div className="pt-3">
+                <label className="inline-flex items-center relative">
+                  <input
+                      type="checkbox"
+                      checked={agreeToPrivacy}
+                      onChange={(e) => setAgreeToPrivacy(e.target.checked)}
+                      className="form-checkbox text-orange-600 peer h-5 w-5 cursor-pointer transition-all appearance-none rounded shadow hover:shadow-md border border-slate-300 checked:bg-orange-600 checked:border-orange-600"
+                  />
+                  <span
+                      className="absolute text-white opacity-0 peer-checked:opacity-100 top-1/2 left-[10px] transform -translate-x-1/2 -translate-y-1/2">
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" viewBox="0 0 20 20" fill="currentColor"
+                       stroke="currentColor" strokeWidth="1">
+                    <path fillRule="evenodd"
+                          d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                          clipRule="evenodd"></path>
+                  </svg>
+                  </span>
+                  <span className="ml-2 text-sm font-medium text-gray-500">
+                    <span className="cursor-pointer underline mr-1">개인정보 수집 및 이용동의</span>
+                    <span className="text-orange-600">(필수)</span>
+                  </span>
+                </label>
+              </div>
+              <div className="mt-2 border border-gray-300 rounded-md h-16 overflow-y-auto p-2 text-sm">
+                <h4 className="text-gray-900 font-semibold">개인정보 수집 및 이용동의</h4>
+                <p className="text-gray-700">
+                  gggggggg
+                  gggggggg
+                  gggggggggggggggg
+                  gggggggg
+                  gggggggg
+                </p>
+              </div>
+
+              <div className="pt-3">
+                <label className="inline-flex items-center relative">
+                  <input
+                      type="checkbox"
+                      checked={agreeToTerms}
+                      onChange={(e) => setAgreeToTerms(e.target.checked)}
+                      className="form-checkbox text-orange-600 peer h-5 w-5 cursor-pointer transition-all appearance-none rounded shadow hover:shadow-md border border-slate-300 checked:bg-orange-600 checked:border-orange-600"
+                  />
+                  <span
+                      className="absolute text-white opacity-0 peer-checked:opacity-100 top-1/2 left-[10px] transform -translate-x-1/2 -translate-y-1/2">
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" viewBox="0 0 20 20"
+                       fill="currentColor"
+                       stroke="currentColor" strokeWidth="1">
+                    <path fillRule="evenodd"
+                          d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                          clipRule="evenodd"></path>
+                  </svg>
+                  </span>
+                  <span className="ml-2 text-sm font-medium text-gray-500">
+                    <span className="cursor-pointer underline mr-1">이용약관 동의</span>
+                    <span className="text-orange-600">(필수)</span>
+                  </span>
+                </label>
+              </div>
+
+              <div className="mt-2 border border-gray-300 rounded-md h-16 overflow-y-auto p-2 text-sm">
+                <h4 className="text-gray-900 font-semibold">이용약관</h4>
+                <p className="text-gray-700">
+                  gggggggg
+                  gggggggg
+                  gggggggggggggggg
+                  gggggggg
+                  gggggggg
+                </p>
+              </div>
+
+              {errors.terms && <p className="text-red-500 text-sm mt-2">{errors.terms}</p>}
+
+              <div className="pt-4">
+                <button type="submit"
+                        className="flex w-full justify-center rounded-md bg-orange-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-orange-400">
+                  회원가입
+                </button>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    </ModalLayout>
+  );
 };
 
 export default SignupModal;
